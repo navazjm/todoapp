42e2a7f703fb7fdcc4ff79f78161640608c95e84
navazjm <michaelnavs@gmail.com>
2023-01-23 00:34:45 -0600

    client - updated .env api url

--- todoapp-client/src/lib/axios.ts
+++ todoapp-client/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_API_URL = process.env.API_URL || "http://localhost:8080";
+export const BASE_API_URL = import.meta.env.API_URL || "http://localhost:8080";
 
 export default axios.create({
     baseURL: BASE_API_URL
