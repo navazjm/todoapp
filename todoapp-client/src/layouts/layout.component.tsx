6be99e42b91eef751ec28ff4b53d6a0e80a84e1f
navazjm <michaelnavs@gmail.com>
2023-02-04 21:18:18 -0600

    client - Navbar component code clean up

--- todoapp-client/src/layouts/layout.component.tsx
+++ todoapp-client/src/layouts/layout.component.tsx
@@ -1,14 +1,14 @@
 import { useAlert } from "../components/alert/alert.hooks";
 import { Props } from "../utils/types";
 import AlertComponent from "../components/alert/alert.component";
-import Navbar from "./navbar/navbar.component";
+import NavbarComponent from "./navbar/navbar.component";
 import "./layout.component.css";
 
 export const Layout = ({ children }: Props) => {
     const alertCtx = useAlert();
     return (
         <>
-            <Navbar />
+            <NavbarComponent />
             <main className="layout-main-container">{children}</main>
             {alertCtx?.alert && alertCtx.alert.message !== "" && <AlertComponent />}
         </>
