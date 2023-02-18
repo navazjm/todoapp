f6eb5bb93247702517af5dca1021431870ac76be
navazjm <michaelnavs@gmail.com>
2023-02-18 11:39:31 -0600

    client - fix axios client api url import

--- todoapp-client/src/lib/axios.ts
+++ todoapp-client/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_API_URL = import.meta.env.CLIENT_API_BASE_URL || "http://localhost:8080/v1";
+export const BASE_API_URL = import.meta.env.VITE_CLIENT_API_BASE_URL || "http://localhost:8080/v1";
 
 export default axios.create({
     baseURL: BASE_API_URL
