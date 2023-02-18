bdeaa8cdf6814ff6fa2832a3cf8835cf349a4e3d
navazjm <michaelnavs@gmail.com>
2023-02-18 13:22:33 -0600

    client - update api endpoint for google login axios call

--- todoapp-client/src/pages/login/google-login/google-login.api.ts
+++ todoapp-client/src/pages/login/google-login/google-login.api.ts
@@ -1,7 +1,7 @@
 import axios from "../../../lib/axios";
 
 export async function login(googleCredential: string): Promise<void> {
-    const resp = await axios.post("/google/login", {
+    const resp = await axios.post("/users/google/login", {
         token: googleCredential
     });
     console.log(resp);
